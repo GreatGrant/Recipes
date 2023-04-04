2c8e58f14af8d9584b469e1391af4b6c830d83ec
Great Grant <greatgrant116@gmail.com>
2023-04-04 06:38:35 +0100

    refactor model class

--- lib/Recipe.dart
+++ lib/Recipe.dart
@@ -1,3 +1,12 @@
-class Recipe{
+class Recipe {
+  String label;
+  String imageUrl;
 
+  // TODO: Add servings and ingredients here
+  Recipe(
+      this.label,
+      this.imageUrl
+      );
+// TODO: Add List<Recipe> here
 }
+// TODO: Add Ingredient() here
