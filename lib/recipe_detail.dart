448851ab027e848ae3ea620ce0645aa284bd3bec
Great Grant <greatgrant116@gmail.com>
2023-04-07 15:16:57 +0100

    populate recipe detail page

---
+++ lib/recipe_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:recipes/recipe.dart';
+
+class RecipeDetail extends StatefulWidget{
+  final Recipe recipe;
+
+  const RecipeDetail({
+  Key? key,
+    required this.recipe
+  }): super(key: key);
+  //
+  // @override
+  // State<RecipeDetail>createState() {
+  //   return _RecipeDetailState();
+  // }
+}
+// TODO: Add _RecipeDetailState here
